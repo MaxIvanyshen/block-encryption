ba3bcde5e61814a67ab4b3f54ceeb83a65db465b
MaxIvanyshen <maxivanyshen@gmail.com>
2024-06-04 11:07:11 +0300

    feat: rewrite rsa encoder to work with chunks

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha256"
 	"crypto"
 	"errors"
+	"math"
 	"fmt"
 )
 
@@ -16,6 +17,7 @@ type Encoder interface {
 
 type RSAEncoder struct {
     key *rsa.PrivateKey
+    bits int
 }
 
 var RSAEncoderCreationError = errors.New("Error while creating RSA encoder")
@@ -30,32 +32,89 @@ func NewRSAEncoder(bits int) (*RSAEncoder, error) {
         )
 	}
 
-    return &RSAEncoder{key: key}, nil
+    return &RSAEncoder{key: key, bits: bits}, nil
 }
 
 func (encoder *RSAEncoder) Encode(data []byte) ([]byte, error) {
-    encryptedBytes, err := rsa.EncryptOAEP(
-        sha256.New(),
-        rand.Reader,
-        &encoder.key.PublicKey,
-        data,
-        nil,
-    )
-    if err != nil {
-        return make([]byte, 0), fmt.Errorf(
-            "%v: couldn't encrypt your data: %v", RSAEncodingError, err,
-        )
-    }   
+    sha := sha256.New()
+    randReader := rand.Reader
+    done := make(chan interface{})
 
-    return encryptedBytes, nil
+    defer close(done)
+
+    chunkStream := chunkGenerator(done, data, encoder.bits)
+    out := make([]byte, 0)
+
+    for chunk := range chunkStream {
+
+        encryptedBytes, err := rsa.EncryptOAEP(
+            sha,
+            randReader,
+            &encoder.key.PublicKey,
+            chunk,
+            nil,
+        )
+
+        if err != nil {
+            return make([]byte, 0), fmt.Errorf(
+                "%v: couldn't encrypt your data: %v", RSAEncodingError, err,
+            )
+        }   
+
+        out = append(out, encryptedBytes...)
+    }
+
+
+    return out, nil
 }
 
 func(decoder *RSAEncoder) Decode(data []byte) ([]byte, error) {
-    decryptedBytes, err := decoder.key.Decrypt(nil, data, &rsa.OAEPOptions{Hash: crypto.SHA256})
-    if err != nil {
-        return make([]byte, 0), fmt.Errorf("%v: couldn't decrypt the message: %v", RSADecodingError, err)
+    done := make(chan interface{})
+    defer close(done)
+    chunkStream := chunkGenerator(done, data, decoder.bits)
+
+    out := make([]byte, 0)
+
+    for chunk := range chunkStream {
+        decryptedBytes, err := decoder.key.Decrypt(nil, chunk, &rsa.OAEPOptions{Hash: crypto.SHA256})
+        if err != nil {
+            return make([]byte, 0), fmt.Errorf("%v: couldn't decrypt the message: %v", RSADecodingError, err)
+        }
+
+        out = append(out, decryptedBytes...)
     }
 
-    return decryptedBytes, nil
+
+    return out, nil
+}
+
+func chunkGenerator(done chan interface{}, bytes []byte, bits int) <-chan []byte {
+    stream := make(chan []byte)
+
+    chunkSize := int(math.Floor(float64(bits / 8)))
+    if chunkSize == 0 {
+        chunkSize = 1
+    }
+
+    go func() {
+        defer close(stream)
+        
+        for i := 0; i < len(bytes); i += chunkSize {
+
+            if i + chunkSize >= len(bytes) {
+                chunkSize = len(bytes) - i
+            }
+
+            chunk := bytes[i:i+chunkSize]
+            
+            select {
+            case <-done:
+                return
+            case stream<-chunk:
+            }
+        }
+    }()
+
+    return stream    
 }
 

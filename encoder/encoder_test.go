718ce246f22a7e3f1c9d247794de1a02aa738530
MaxIvanyshen <maxivanyshen@gmail.com>
2024-06-04 11:45:22 +0300

    update test

--- encoder/encoder_test.go
+++ encoder/encoder_test.go
@@ -3,13 +3,24 @@ package encoder
 import "testing"
 
 func TestRSAEncoder_EncodeAndDecode_DataSouldEqual(t *testing.T) {
-    rsa, err := NewRSAEncoder(1024)
+    rsa, err := NewRSAEncoder(3000)
     if err != nil {
         t.Fatalf("an error occured: %v", err)
     }
 
-    msg := "hello world"
-    encoded, err := rsa.Encode([]byte(msg))
+    str := []string{"hello", " world"}
+    data := make([]byte, 0)
+
+    for i := 0; i < 256; {
+        if i % 2 == 0 {
+            data = append(data, []byte(str[0])...)
+            i += len(str[0])
+        } else {
+            data = append(data, []byte(str[1])...)
+            i += len(str[1]) - 1
+        }
+    }    
+    encoded, err := rsa.Encode([]byte(data))
     if err != nil {
         t.Fatalf("an error occured: %v", err)
     }
@@ -19,7 +30,7 @@ func TestRSAEncoder_EncodeAndDecode_DataSouldEqual(t *testing.T) {
     }
 
     decodedStr := string(decoded)
-    if msg != decodedStr {
-        t.Fatalf("strings are not equal. want %s got %s", msg, decodedStr)
+    if string(data) != decodedStr {
+        t.Fatalf("strings are not equal. want %s got %s", string(data), decodedStr)
     }
 }
